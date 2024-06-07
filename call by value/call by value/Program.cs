db87238082b44c07df6c445656e0b283893bea53
sudharsanan <ragusudharsanan1@gmail.com>
2024-06-07 17:31:37 +0530

    Added call by value

---
+++ call by value/call by value/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace callbyvalue
+
+// call by value 
+// call by refference
+//over ride
+
+
+{
+    class program
+    {
+    }
+
+
+    //{
+    ////Call By Value In C# 
+    //static void Main(string[] args)
+    //{
+    //    int a = 1;
+    //    int b = 2;
+    //    valuve(a, b);
+    //    Console.WriteLine("a =" + a);
+    //    Console.WriteLine("b =" + b);
+    //}
+    //public static void valuve(int x, int y)
+    //{
+    //    Console.WriteLine("xs =" + x);
+    //    Console.WriteLine("ys =" + y);
+    //    int not = x;
+    //    x = y;
+    //    y = not;
+    //    Console.WriteLine("x =" + x);
+    //    Console.WriteLine("y =" + y);
+    //}
+    ////   }
+
+
+
+    ////{
+    //Call By reference In C# 
+    //static void Main(string[] args)
+    //{
+    //    int a = 10;
+    //    int b = 20;
+
+    //    Console.WriteLine("a(before swap) =" + a);
+    //    Console.WriteLine("b (before swap)=" + b);
+    //    valuve(ref a, ref b);
+
+    //    Console.WriteLine("a(after swap) =" + a);
+    //    Console.WriteLine("b (after swap) =" + b);
+    //  }
+
+    //class abc
+    //{        public static void valuve(ref int x, ref int y)
+    //    {
+    //        Console.WriteLine("xs =" + x);
+    //         Console.WriteLine("ys =" + y);
+    //        int not = x;
+    //        x = y;
+    //        y = not;
+    //        Console.WriteLine("x(after swapin) =" + x);
+    //        Console.WriteLine("y(after swapin) =" + y);
+    //    }
+    //    //   }
+
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Animal myAnimal = new Animal();
+    //        Animal myDog = new Dog();
+    //        Animal myparrot=new Parrot();
+    //        myAnimal.animalSound();
+    //        myDog.animalSound();
+    //        myparrot.animalSound();
+
+    //    }
+    //}
+    //class Animal  // (parent) 
+    //{
+    //    public virtual void animalSound()
+    //    {
+    //        Console.WriteLine("The animal makes a sound");
+    //    }
+    //}
+    //    class Dog : Animal  // (child) 
+    //    {
+    //        public override void animalSound()
+    //        {
+    //            Console.WriteLine("The dog says: bow wow");
+    //        }
+    //    }
+    //class Parrot : Animal  //(child) 
+    //{
+    //    public override void animalSound()
+    //    {
+    //        Console.WriteLine("The parrot says: ke ke");
+    //    }
+    //}
+
+
+
+}
